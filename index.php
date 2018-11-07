f785a2aa3b74fb0b72ddbaf3a24f5fa00e09fad1
etefft <erich.tefft@gmail.com>
2018-11-07 13:57:11 -0700

    new files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require("components/header.php");
+    require("components/nav.php");
+?>
+
+<div id="container">
+
+</div>
+
+<?php
+
+    require("components/footer.php");
+
+?>
---
+++ js/main.js
@@ -0,0 +1,3 @@
+// Main javascript folder
+
+console.log("test to make sure this page is connected")
